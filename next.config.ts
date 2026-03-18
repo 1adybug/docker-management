9560fc47bb472945c0150190764786df00d78597
陆子虚 <lurongv@qq.com>
2026-03-18 16:55:35 +0800

    ✨ feature: body size 限制调整至 10GB

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import { NextConfig } from "next"
 
 const config: NextConfig = {
+    experimental: {
+        proxyClientMaxBodySize: 1024 * 1024 * 1024 * 10,
+    },
     output: process.env.NEXT_OUTPUT as "standalone" | "export" | undefined,
     outputFileTracingIncludes: {
         "/*": ["./node_modules/7zip-bin/**/*", "./node_modules/.pnpm/7zip-bin@*/node_modules/7zip-bin/**/*"],
