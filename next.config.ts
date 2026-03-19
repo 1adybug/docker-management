92760f9988eec3479e09131932464d670a5d75ef
陆子虚 <lurongv@qq.com>
2026-03-19 10:23:50 +0800

    ✨ feature: add serverActions body size limit to experimental config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { NextConfig } from "next"
 const config: NextConfig = {
     experimental: {
         proxyClientMaxBodySize: 1024 * 1024 * 1024 * 10,
+        serverActions: {
+            bodySizeLimit: 1024 * 1024 * 1024 * 10,
+        },
     },
     output: process.env.NEXT_OUTPUT as "standalone" | "export" | undefined,
     outputFileTracingIncludes: {
