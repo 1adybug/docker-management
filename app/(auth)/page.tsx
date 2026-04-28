e9a35f3474db900819544164a6a7df4c902aa033
陆子虚 <lurongv@qq.com>
2026-04-28 10:20:13 +0800

    🐞 fix: 统一页面标题配置

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react"
 
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "首页",
+}
+
 const Page: FC = () => <div>Hello, World!</div>
 
 export default Page
